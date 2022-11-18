646b8bb58d4c1f416ad26025ee9f6d1c41cd8b1a
mjsky-gulshan96 <gulshan1212kumar@gmail.com>
2022-11-18 14:59:11 +0530

    Update-2

--- index.js
+++ index.js
@@ -9,15 +9,14 @@
             posts.user.forEach(items => {
                 document.querySelector('tbody').innerHTML += `
                     <tr>
-                        < id="update_form">
-                            <td><input type="text" id="name-${items.id}" value="${items.name}" class="outline" required /></td>
-                            <td><input type="text" id="last-${items.id}" value="${items.last}" class="outline" required /></td>
-                            <td><input type="tel" id="tel-${items.id}" value="${items.Contact}" class="outline" required /></td>
-                            <td>
-                                <button id="edit-${items.id}" class="button-click btn btn-outline-primary" onclick="editCol('${items.id}')">Edit</button>
-                                <button id="save-${items.id}" class="button-click btn btn-outline-success d-none" onclick="updateCol('${items.id}')">Save</button>
-                                <button class="button-click btn btn-outline-danger" onclick="deleteCol('${items.id}')" >Delete</button>
-                            </td>
+                        <td><input type="text" id="name-${items.id}" value="${items.name}" name="update_name" class="outline" required /></td>
+                        <td><input type="text" id="last-${items.id}" value="${items.last}" name="update_last" class="outline" required /></td>
+                        <td><input type="tel" id="tel-${items.id}" value="${items.Contact}" name="update_contact" class="outline" required /></td>
+                        <td>
+                            <button id="edit-${items.id}" class="button-click btn btn-outline-primary" onclick="editCol('${items.id}')">Edit</button>
+                            <button id="save-${items.id}" class="button-click btn btn-outline-success d-none" onclick="updateCol('${items.id}')">Save</button>
+                            <button class="button-click btn btn-outline-danger" onclick="deleteCol('${items.id}')" >Delete</button>
+                        </td>                      
                     </tr>
                 `;
             });
@@ -68,28 +67,45 @@ function editCol(id) {
 
 function updateCol(id) {
     var data = {};
-    data.name = $('#name-'+id).val();
-    data.last = $('#last-'+id).val();
-    data.Contact = $('#tel-'+id).val();
+    var update_name = $('#name-'+id).val();
+    var update_last = $('#last-'+id).val();
+    var update_contact = $('#tel-'+id).val(); 
 
-    var dataObj = JSON.stringify(data);
-    $.ajax({
-    url: "http://localhost:3000/user/"+id,
-    method: "PUT",
-    data: dataObj,
-    contentType: 'application/json; charset=utf-8',
-    success: function (){
-        alert('Successfully updated');
-    },
-    error: function (e) {
-        console.log(e);
+    var regex_update_name = new RegExp("^[a-zA-Z]*$").test(update_name);
+    var regex_update_last = new RegExp("^[a-zA-Z]*$").test(update_last);
+    var regex_update_contact = new RegExp("^[6-9]{1}[0-9]{9}$").test(update_contact);
+
+    if(update_name.length > 0 && update_last.length > 0 && update_contact.length > 0) {
+        if(regex_update_name && regex_update_last && regex_update_contact) {
+            data.name = update_name;
+            data.last = update_last;
+            data.Contact =update_contact;
+
+            var dataObj = JSON.stringify(data);
+            updateForm();
+            $.ajax({
+            url: "http://localhost:3000/user/"+id,
+            method: "PUT",
+            data: dataObj,
+            contentType: 'application/json; charset=utf-8',
+            success: function (){
+                alert('Successfully updated');
+            },
+            error: function (e) {
+                console.log(e);
+            }
+            })
+            $("#save-"+id).addClass('d-none');
+            $("#edit-"+id).removeClass('d-none');
+            $('#name-'+id).addClass('outline');
+            $('#last-'+id).addClass('outline');
+            $('#tel-'+id).addClass('outline');
+        } else {
+            alert("Fill Right Information");
+        }
+    }else {
+        alert("Fill the Information");
     }
-    })
-    $("#save-"+id).addClass('d-none');
-    $("#edit-"+id).removeClass('d-none');
-    $('#name-'+id).addClass('outline');
-    $('#last-'+id).addClass('outline');
-    $('#tel-'+id).addClass('outline');
 }
 
 function addCol() {
@@ -128,25 +144,33 @@ function submitPopUp() {
     var last = $('#form_last').val();
     var contact = $('#form_contact').val(); 
 
-    if(name.length > 0 && last.length > 0 && contact > 0) {
-        data.name = name.val();
-        data.last = last.val();
-        data.Contact = contact.val();
-        var dataObj = JSON.stringify(data);
-        $.ajax({
-            url: "http://localhost:3000/user/",
-            method: "POST",
-            data: dataObj,
-            contentType: 'application/json; charset=utf-8',
-            success: function(){
-                $('.address_popup').addClass('d-none');
-            },
-            error: function (e) {
-                console.log(e);
-            }
-        });
-    }else{
-        alert('Fill Information');
+    data.name = name;
+    data.last = last;
+    data.Contact = contact;
+    var dataObj = JSON.stringify(data);
+    var regex_name = new RegExp("^[a-zA-Z]*$").test(name);
+    var regex_last = new RegExp("^[a-zA-Z]*$").test(last);
+    var regex_contact = new RegExp("^[6-9]{1}[0-9]{9}$").test(contact);
+    if(name.length > 0 && last.length > 0 && contact.length > 0) {
+        if(regex_name && regex_last && regex_contact) {
+            $.ajax({
+                url: "http://localhost:3000/user",
+                method: "POST",
+                data: dataObj,
+                contentType: 'application/json; charset=utf-8',
+                success: function(){
+                    $('.address_popup').addClass('d-none');
+                    alert("Successfull Added");
+                },
+                error: function (e) {
+                    console.log(e);
+                }
+            });
+        } else {
+            alert("Fill Right Information");
+        }
+    }else {
+        alert("Fill the Information");
     }
 }
 
